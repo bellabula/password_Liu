7daba63910b2956d5e7ab531db13d15363569301
bellabula <zz225864@gmail.com>
2021-03-15 13:01:15 +0800

    first commit

--- password.py
+++ password.py
@@ -1,12 +1,12 @@
 set_password = 'a123456'
 n = 1
 while n <= 3:
-	password = input('請輸入密碼: ')
-	if password == set_password:
-		print('登入成功')
-		break
-	else:
-		print('密碼錯誤! 還有', 3-n, '次機會')
-	n += 1
+    password = input('請輸入密碼: ')
+    if password == set_password:
+        print('登入成功')
+        break
+    else:
+        print('密碼錯誤! 還有', 3-n, '次機會')
+    n += 1
 if n > 3:
-	print('登入失敗, 請用原信箱重設密碼')
+    print('登入失敗, 請用原信箱重設密碼')
